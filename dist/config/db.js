2b1212a4ab88d8819fc6ba41443013da399a6f0b
Ebenezer Aladesuyi <ebenezeraladesuyi@gmail.com>
2023-07-26 20:28:44 +0100

    newest

--- dist/config/db.js
+++ dist/config/db.js
@@ -13,10 +13,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 // const DB_URL = "mongodb://0.0.0.0:27017/forEben";
 // const DB_URL = "mongodb://127.0.0.1/Eben";
-// const localUrl = "mongodb+srv://eben1909:victoria-19@cluster0.u3wri8y.mongodb.net/?retryWrites=true&w=majority";
-const MONGOOSE_DB = "mongodb://127.0.0.1:27017/istgth";
+// const MONGOOSE_DB = "mongodb+srv://eben1909:victoria-19@cluster0.u3wri8y.mongodb.net/?retryWrites=true&w=majority";
+const MONGOOSE_DB = "mongodb+srv://eben19:ebenezer19@cluster0.u3wri8y.mongodb.net/?retryWrites=true&w=majority";
+// const url = process.env.MONGOOSE_DB;
 const dbConfig = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const conn = yield mongoose_1.default.connect(MONGOOSE_DB);
@@ -27,3 +30,15 @@ const dbConfig = () => __awaiter(void 0, void 0, void 0, function* () {
     }
 });
 exports.default = dbConfig;
+// const dbConfig = () => {
+//     try {
+//       mongoose
+//         .connect(MONGOOSE_DB)
+//         .then(() => {
+//           console.log('connected to database on port');
+//         })
+//     } catch (error) {
+//       console.log(`failed to connect to database` , error);
+//     }
+//   };
+//   export default dbConfig;
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
+import env from "dotenv";
+env.config()
 
 // const DB_URL = "mongodb://0.0.0.0:27017/forEben";
-// const DB_URL = "mongodb://127.0.0.1/Eben";
+// const DB_URL = "mongodb://127.0.0.1/Eben"
 
-const MONGOOSE_DB = "mongodb+srv://eben1909:victoria-19@cluster0.u3wri8y.mongodb.net/?retryWrites=true&w=majority";
-
-// const MONGOOSE_DB = "mongodb://127.0.0.1:27017/istgth"
-
-
+const MONGOOSE_DB = "mongodb+srv://eben19:ebenezer19@cluster0.u3wri8y.mongodb.net/?retryWrites=true&w=majority"
 
 
 const dbConfig = async (): Promise<void> => {
@@ -20,3 +18,17 @@ const dbConfig = async (): Promise<void> => {
   };
   
   export default dbConfig;
+
+// const dbConfig = () => {
+//     try {
+//       mongoose
+//         .connect(MONGOOSE_DB)
+//         .then(() => {
+//           console.log('connected to database on port');
+//         })
+//     } catch (error) {
+//       console.log(`failed to connect to database` , error);
+//     }
+//   };
+
+//   export default dbConfig;
